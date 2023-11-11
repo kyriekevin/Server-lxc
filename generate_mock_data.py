37713ae00ce41330a3116384729ab49700f672da
kyriekevin <2718629413@qq.com>
2023-11-11 20:35:22 +0800

    [generate_mock_data.py] Refactor code to handle individual mock JSON generation and improve file structure organization

--- generate_mock_data.py
+++ generate_mock_data.py
@@ -1,62 +1,81 @@
-import pandas as pd
-import numpy as np
+import os
+import json
+import random
+from typing import Dict, List, Tuple
+
+gt_folder = "./img_test/"
+mock_data_folder = "mock_data"
 
 
-def generate_ground_truth(num_images=10, max_defects_per_image=3):
-    ground_truth_data = []
-    for i in range(num_images):
-        num_defects = np.random.randint(1, max_defects_per_image + 1)
-        for _ in range(num_defects):
-            ground_truth_data.append(
-                {
-                    "image_id": f"image{i}",
-                    "x1": np.random.randint(0, 100),
-                    "y1": np.random.randint(0, 100),
-                    "x2": np.random.randint(100, 150),
-                    "y2": np.random.randint(100, 150),
-                    "defect_type": np.random.choice(["defect1", "defect2", "defect3"]),
+def parse_gt_json(json_file_path: str) -> Tuple[str, List[Dict]]:
+    """
+    Parses the ground truth JSON file and extracts the object data, along with the image file name.
+
+    Args:
+    json_file_path (str): The file path of the JSON file.
+
+    Returns:
+    Tuple[str, List[Dict]]: A tuple where the first element is the image file name associated with this JSON,
+                            and the second element is a list of ground truth data (each item is a dictionary representing an object).
+    """
+    with open(json_file_path, "r") as file:
+        data = json.load(file)
+
+    objects = data.get("step_1", {}).get("result", [])
+    image_file_name = os.path.basename(json_file_path).rsplit(".", 1)[0]
+
+    return image_file_name, objects
+
+
+def parse_gt_folder(folder_path: str) -> Dict[str, List[Dict]]:
+    gt_data = {}
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".json"):
+                file_path = os.path.join(root, file)
+                image_file_name, objects = parse_gt_json(file_path)
+                gt_data[image_file_name] = objects
+
+    return gt_data
+
+
+def generate_individual_mock_jsons(gt_data, mock_data_folder):
+    if not os.path.exists(mock_data_folder):
+        os.makedirs(mock_data_folder)
+
+    for image_file_name, objects in gt_data.items():
+        mock_objects = []
+        for obj in objects:
+            choice = random.random()
+            if choice < 0.8:
+                mock_obj = {
+                    "x": obj["x"] + random.randint(-10, 10),
+                    "y": obj["y"] + random.randint(-10, 10),
+                    "width": obj["width"] + random.randint(-10, 10),
+                    "height": obj["height"] + random.randint(-10, 10),
+                    "attribute": obj["attribute"],
                 }
-            )
-    return pd.DataFrame(ground_truth_data)
+                mock_objects.append(mock_obj)
 
-
-def generate_inference(
-    num_images=10,
-    avg_defects_per_image=4,
-    algorithm_types=("algo1", "algo2", "algo3", "algo4"),
-):
-    inference_data = []
-    # Determine total inference time for each algorithm type
-    total_inference_time = {algo: np.random.uniform(1, 10) for algo in algorithm_types}
-
-    for i in range(num_images):
-        num_defects = np.random.poisson(avg_defects_per_image)
-        for _ in range(num_defects):
-            algo_type = np.random.choice(algorithm_types)
-            inference_data.append(
-                {
-                    "image_id": f"image{i}",
-                    "x1": np.random.randint(0, 100),
-                    "y1": np.random.randint(0, 100),
-                    "x2": np.random.randint(100, 150),
-                    "y2": np.random.randint(100, 150),
-                    "defect_type": np.random.choice(["defect1", "defect2", "defect3"]),
-                    "algorithm_type": algo_type,
-                    "total_inference_time": total_inference_time[
-                        algo_type
-                    ],  # Use the fixed total time for this algorithm
+            # 随机生成一些额外的对象
+            extra_predictions_rate = random.randint(0, 10)
+            for _ in range(extra_predictions_rate):
+                mock_obj = {
+                    "x": random.randint(0, 1000),
+                    "y": random.randint(0, 1000),
+                    "width": random.randint(10, 1000),
+                    "height": random.randint(10, 1000),
+                    "attribute": obj["attribute"],
                 }
-            )
-    return pd.DataFrame(inference_data)
+                mock_objects.append(mock_obj)
+
+        mock_data = {"image_name": image_file_name, "objects": mock_objects}
+
+        # 写入模拟数据到文件
+        output_file_path = os.path.join(mock_data_folder, image_file_name + ".json")
+        with open(output_file_path, "w", encoding="utf-8") as file:
+            json.dump(mock_data, file, indent=4, ensure_ascii=False)
 
 
-# Generate ground truth and inference data
-gt_df = generate_ground_truth()
-inference_df = generate_inference()
-
-# Save the data to CSV files
-gt_df.to_csv("ground_truth.csv", index=False)
-inference_df.to_csv("inference_results.csv", index=False)
-
-print("Ground Truth saved to 'ground_truth.csv'")
-print("Inference Results saved to 'inference_results.csv'")
+gt_data = parse_gt_folder(gt_folder)
+generate_individual_mock_jsons(gt_data, mock_data_folder)
