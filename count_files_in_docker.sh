917ea26cc268c0d4a10f0668d96ec5082735cda0
kyriekevin <2718629413@qq.com>
2024-01-22 14:42:50 +0800

    update count files in docker shell

--- count_files_in_docker.sh
+++ count_files_in_docker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 <docker_container_name_or_id> <path_to_directory>"
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <docker_container_name_or_id> "
 	exit 1
 fi
 
 CONTAINER_NAME_OR_ID=$1
-DIRECTORY_PATH=$2
 
-FILE_COUNT=$(docker exec $CONTAINER_NAME_OR_ID sh -c "find $DIRECTORY_PATH -maxdepth 1 -type f | wc -l")
+FILE_COUNT=$(docker exec $CONTAINER_NAME_OR_ID sh -c "ls /home/result | wc -l")
+# 获取文件总数
+# ls eval_data/images | wc -l
 
 echo $FILE_COUNT
 
