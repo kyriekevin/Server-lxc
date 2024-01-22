cf2519a397495a849e8990092a3fb44bca5be7f7
kyriekevin <2718629413@qq.com>
2024-01-22 13:44:12 +0800

    update docker shell

--- docker.sh
+++ docker.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 <path_to_tar_file> <gpu_id> <cpu_cores> <mount_path> <folder_in_container>"
-    exit 1
+if [ "$#" -ne 6 ]; then
+	echo "Usage: $0 <path_to_tar_file> <gpu_id> <cpu_cores> <mount_path> <taskID> <userID>"
+	exit 1
 fi
 
 TAR_PATH="$1"
 GPU_ID="$2"
 CPU_CORES="$3"
 MOUNT_PATH="$4"
-FOLDER_IN_CONTAINER="$5"
+TASKID="$5"
+USERID="$6"
 
 if [ ! -f "$TAR_PATH" ]; then
-    echo "Error: File not found!"
-    exit 1
+	echo "Error: File not found!"
+	exit 1
 fi
 
 echo "Loading Docker image from $TAR_PATH..."
@@ -21,36 +22,33 @@ IMAGE_ID=$(sudo docker load -i $TAR_PATH | awk '{print $3}')
 echo "Image loaded with ID: $IMAGE_ID"
 
 echo "Starting the Docker container with mounted path $MOUNT_PATH..."
-sudo docker run -d --gpus "\"device=$GPU_ID\"" --cpuset-cpus="$CPU_CORES" -m 32g -v $MOUNT_PATH:$MOUNT_PATH $IMAGE_ID tail -f /dev/null
+sudo docker run -d --gpus "\"device=$GPU_ID\"" --cpuset-cpus="$CPU_CORES" -m 32g -v /data/$TASKID/eval_data/images:$MOUNT_PATH $IMAGE_ID tail -f /dev/null
 
 if [ $? -ne 0 ]; then
-    echo "Failed to start the Docker container."
-    exit 1
+	echo "Failed to start the Docker container."
+	exit 1
 fi
 
 CONTAINER_ID=$(sudo docker ps -l -q)
 
 if [ -z "$CONTAINER_ID" ]; then
-    echo "Failed to retrieve the container ID."
-    exit 1
+	echo "Failed to retrieve the container ID."
+	exit 1
 fi
 echo "Container started with ID: $CONTAINER_ID"
 
-echo "Creating folder $FOLDER_IN_CONTAINER in the Docker container..."
-sudo docker exec $CONTAINER_ID mkdir -p $FOLDER_IN_CONTAINER
-if [ $? -ne 0 ]; then
-    echo "Error creating folder in the container."
-    exit 1
-fi
-echo "Folder created successfully."
-
 echo "Executing command in the Docker container..."
-sudo docker exec $CONTAINER_ID /bin/bash -c "source /root/miniconda3/etc/profile.d/conda.sh && conda activate pytorch && python /root/resnet.py"
+sudo docker exec $CONTAINER_ID /bin/bash -c "bash /home/run.sh"
 if [ $? -ne 0 ]; then
-    echo "Error executing command in the container."
-    exit 1
+	echo "Error executing command in the container."
+	exit 1
 fi
 echo "Command executed successfully."
+
+echo "Copying the result from the Docker container..."
+mkdir -p /data/$TASKID/
+sudo docker cp $CONTAINER_ID:/home/result /data/$TASKID/$USERID/
+echo "Result copied to /data/$TASKID/$USERID/"
 
 echo "Stopping and removing the Docker container..."
 sudo docker stop $CONTAINER_ID
@@ -60,4 +58,3 @@ echo "Container stopped and removed."
 echo "Removing the Docker image..."
 sudo docker rmi $IMAGE_ID
 echo "Image removed."
-
